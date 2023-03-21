0dd6198322cee64aa3ad3014d0592f91ba223357
Eshban Suleman <eshban9492@gmail.com>
2023-03-21 14:02:08 +0500

    Initialized Board Logic

---
+++ src/board.rs
@@ -0,0 +1,11 @@
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub enum PieceColor {
+    White,
+    Black,
+}
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct GamePiece {
+    pub color: PieceColor,
+    pub crowned: bool,
+}
